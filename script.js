8550d2e4fe6a3aeb37dc3835c033da9295f9fed8
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-10-28 16:15:05 +0530

    added a few basic routes and statics

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // npm i ejs express 
-
 const express = require('express');
 
 const app = express();
 
 app.set("view engine","ejs"); // sets the view engine to ejs
+app.use(express.static('./public'));
 
 app.get('/',function(req,res){
     res.render('index');      // here we use res.render() instead of res.send() coz we passing the whole template instead of hardcoded template
@@ -15,6 +15,10 @@ app.get('/about',function(req,res){
 app.get('/profile/:username',function(req,res){
     res.send(`response from ${req.params.username} profile page`) //here we can access the params object passed into the req from the url
     
+})
+
+app.get('/error',function(req,res){
+    res.render('error');
 })
 
 
